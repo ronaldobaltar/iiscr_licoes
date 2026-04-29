16a7900b186ef272ce6bacd23942d968f46093b1
Ronaldo Baltar <ronaldo.baltar@gmail.com>
2026-04-29 14:33:21 -0300

    Funcionou - Início do Teste do uso do Google forms com Swirl

---
+++ config_swirl_googleform_test.R
@@ -0,0 +1,14 @@
+# https://github.com/seankross/Google_Forms_Course#
+# Google form:
+# https://docs.google.com/forms/d/e/1FAIpQLSdDOjII-iA9QCbKG-sUg2AQ7U6x5S6fkzXrCXNu-Maxc_5t9w/viewform?usp=pp_url
+
+library(swirl)
+swirl()
+install_course_zip("Google_Forms_Course-master.zip")
+install_course_directory("C:/Users/ronal/OneDrive/rprojs/iiscr_licoes/swirl_curso")
+
+install_course_github("seankross", "Google_Forms_Course")
+
+uninstall_all_courses()
+
+
