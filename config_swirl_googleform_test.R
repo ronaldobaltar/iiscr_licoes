79f9d6bb10f25a6a3ccdf6af5cc2c1775bc290fe
Ronaldo Baltar <ronaldo.baltar@gmail.com>
2026-04-29 14:53:16 -0300

    Feita a leitura do csv com resposta do questionário OK

--- config_swirl_googleform_test.R
+++ config_swirl_googleform_test.R
@@ -1,6 +1,11 @@
 # https://github.com/seankross/Google_Forms_Course#
 # Google form:
 # https://docs.google.com/forms/d/e/1FAIpQLSdDOjII-iA9QCbKG-sUg2AQ7U6x5S6fkzXrCXNu-Maxc_5t9w/viewform?usp=pp_url
+# Tem que ter o número da questão no final. Antes de gerar o código de envio automático no Google Form, tem 
+# que inserir algum dado no campo, para que o 'entry.' do campo seja identificado:
+#
+# "https://docs.google.com/forms/d/e/1FAIpQLSdDOjII-iA9QCbKG-sUg2AQ7U6x5S6fkzXrCXNu-Maxc_5t9w/viewform?usp=pp_url&entry.926332692"
+# Depois deleta a resposta inserida só para gerar o link, ou seja, o que vem depois do últmo =
 
 library(swirl)
 swirl()
@@ -12,3 +17,7 @@ install_course_github("seankross", "Google_Forms_Course")
 uninstall_all_courses()
 
 
+# Decode
+# baixar csv do Gooble Forms
+
+resposta_v2 <- swirlify::google_form_decode()
